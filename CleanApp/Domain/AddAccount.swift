9e17512224406011503eca884f2cbdce99a06023
Gabriela Leal <gabrielamiranda@alu.ufc.br>
2023-01-25 18:07:49 -0300

    feat: add AccountModel usecase

--- CleanApp/Domain/AddAccount.swift
+++ CleanApp/Domain/AddAccount.swift
@@ -6,14 +6,3 @@
 //
 
 import Foundation
-
-protocol AddAccount {
-    func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
-}
-
-struct AddAccountModel {
-    var name: String
-    var email: String
-    var password: String
-    var passwordConfirmation: String
-}
